3eafbd2dce9d62f6dc0b1b0d03a5b87bce636306
Varsha1793 <100304556+Varsha1793@users.noreply.github.com>
2022-04-09 22:37:48 +0530

    Add files via upload

--- ex7.py
+++ ex7.py
@@ -1,15 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas
+import connect
+import ex7(2)
 url="https://www.careerindia.com/colleges/colleges-in-chennai/"
+parser.add_argument("--dbname",help="Enter the number of pages to parse",type=int)
 
+info=[]
+
+connect.connect(args.dbname)
 for page in range(1,4):
     request_ = requests.get(url)
     content=request_.content
     soup=BeautifulSoup(content,"html.parser")
     
     colleges=soup.find_all("div",{"class":"edu-detlist-container"})
-    info=[]
+
     for college in colleges:
         college_dictionary={}
         college_dictionary["name"]=college.find("h2",{"class":"edu-det-heading"}).text
@@ -19,7 +25,9 @@ for page in range(1,4):
         except AttributeError:
                 pass
         info.append(college_dictionary)
+        connect.inserting(agrs.dbname,college_dictionary.values())
 dataframe=pandas.DataFrame(info)
 dataframe.to_csv("college.csv")
+connect.information(args.dbname)
 
         
